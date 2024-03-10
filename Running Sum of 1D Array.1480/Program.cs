433a91cccf4c732f5bfa49732e0da60c56a04a23
PovilasKulevicius <kulevicius.povilas@gmail.com>
2024-03-10 18:16:52 +0200

    Extracted into methods

--- Running Sum of 1D Array.1480/Program.cs
+++ Running Sum of 1D Array.1480/Program.cs
@@ -1,31 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-internal class LeetCode
+﻿class LeetCode
 {
     static void Main()
     {
-        Console.WriteLine("Enter numbers separated only with comma:");
-        var numberStrings = Console.ReadLine()?.Split(',');
-        if (numberStrings == null)
-        {
-            throw new ArgumentException("No input");
-        }
+        var numbers = GetInitialNumbers();
+        var results = CalculateRunningSum(numbers);
+        OutputResult(results);
+    }
 
-        var numbers = Array.ConvertAll(numberStrings, int.Parse);
-
-        var results = new List<int>();
-        for (var i = 0; i < numbers.Length; i++)
-        {
-            if (results.Count == 0)
-            {
-                results.Add(numbers[i]);
-            }
-            else
-            {
-                results.Add(results[i - 1] + numbers[i]);
-            }
-        }
-
+    private static void OutputResult(List<int> results)
+    {
         for (var i = 0; i < results.Count; i++)
         {
             Console.Write(results[i]);
@@ -34,5 +17,30 @@ internal class LeetCode
                 Console.Write(", ");
             }
         }
+    }
+
+    private static List<int> CalculateRunningSum(int[] numbers)
+    {
+        var results = new List<int>();
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            if (results.Count == 0)
+                results.Add(numbers[i]);
+            else
+                results.Add(results[i - 1] + numbers[i]);
+        }
+
+        return results;
+    }
+
+    private static int[] GetInitialNumbers()
+    {
+        Console.WriteLine("Enter numbers separated only with comma:");
+        var numberStrings = Console.ReadLine()?.Split(',');
+        if (numberStrings == null)
+            throw new ArgumentException("No input");
+
+        var numbers = Array.ConvertAll(numberStrings, int.Parse);
+        return numbers;
     }
 }
