dcd732309274c91dc502a4c028e35fa154d4a2db
PovilasKulevicius <kulevicius.povilas@gmail.com>
2024-03-17 21:24:04 +0200

    Solved 412. Fizz Buzz

--- 412. Fizz Buzz/Program.cs
+++ 412. Fizz Buzz/Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.VisualBasic;
 
-Console.WriteLine("Hello, World!");
+namespace _412._Fizz_Buzz;
+
+abstract class Solution
+{
+    static void Main()
+    {
+        var input = GetInput();
+        var results = PlayFizzBuzz(input);
+        OutputResult(results);
+        
+    }
+
+    private static void OutputResult(string[] results)
+    {
+        var i = 0;
+        foreach (var result in results)
+        {
+            Console.Write(result);
+            
+            if (i != results.Length - 1)
+                Console.Write(", ");
+            
+            i++;
+        }
+    }
+
+    private static string[] PlayFizzBuzz(int input)
+    {
+        var result = new string[input];
+        for (var i = 1; i <= input; i++)
+        {
+            var outputIndex = i - 1;
+            
+            if (i % 3 == 0 && (i % 5 == 0))
+                result[outputIndex] = "FizzBuzz";
+            else if (i % 3 == 0)
+                result[outputIndex] = "Fizz";
+            else if (i % 5 == 0)
+                result[outputIndex] = "Buzz";
+            else
+                result[outputIndex] = i.ToString();
+        }
+
+        return result;
+    }
+
+    private static int GetInput()
+    {
+        return 15;
+    }
+}
