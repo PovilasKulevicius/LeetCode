804ca374b12c22c5bb7392e4ef6700b865ab7fc4
PovilasKulevicius <kulevicius.povilas@gmail.com>
2024-04-01 13:54:36 +0300

    Solved 58. Length of Last Word and 1. Two Sum

---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _1._Two_Sum;
+
+abstract class Solution
+{
+    static void Main()
+    {
+        var (nums, target) = GetInitialData();
+        var results = TwoSum(nums, target);
+        OutputResult(results);
+        
+    }
+
+    private static void OutputResult(int[] results)
+    {
+        foreach (var num in results)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    private static int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+
+        return [];
+    }
+
+    private static (int[], int) GetInitialData()
+    {
+        var nums = new[] { 3,2,4 };
+        return (nums, 6);
+    }
+}
---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _58._Length_of_Last_Word;
+
+abstract class Solution
+{
+    static void Main()
+    {
+        var words = GetInitialData();
+        var results = LengthOfLastWord(words);
+        OutputResult(results);
+        
+    }
+
+    private static void OutputResult(int result)
+    {
+        Console.Write(result);
+    }
+
+    private static int LengthOfLastWord(string s)
+    {
+        var seperatedWords = s.Split(' ');
+        var lastWord = seperatedWords.Last(x => x != "");
+        return lastWord.Length;
+    }
+
+    private static string GetInitialData()
+    {
+        return "   fly me   to   the moon  ";
+    }
+}
