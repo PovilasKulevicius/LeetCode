a6b45ac07575b829fd0c28b76ca73b912cbb8c1b
PovilasKulevicius <kulevicius.povilas@gmail.com>
2024-04-05 20:51:26 +0300

    Added solution for 1544. Make The String Great

---
+++ 1544. Make The String Great/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace _1544._Make_The_String_Great;
+
+abstract class Solution
+{
+    static void Main()
+    {
+        var s = GetInitialData();
+        var results = MakeGood(s);
+        OutputResult(results);
+        
+    }
+
+    private static string MakeGood(string s)
+    {
+        for (var i = 1; i < s.Length; i++)
+        {
+            var previous = s[i - 1];
+            var current = s[i];
+            
+            var wasLastCharacterLowerCase = char.IsLower(previous);
+            
+            if (char.ToLower(previous) == char.ToLower(current) && char.IsLower(current) != wasLastCharacterLowerCase)
+            {
+                s = s.Substring(0, i-1) + s.Substring(i + 1, s.Length - i - 1);
+                i = 0;
+            }
+        }
+        
+        return s;
+    }
+
+    private static string GetInitialData()
+    {
+        return "jeSsEJ";
+    }
+    
+    private static void OutputResult(string result)
+    {
+        Console.WriteLine(result);
+    }
+}
